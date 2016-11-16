28bf1df67836b3d61e0edd7cd7486d2e6ead5bad
Eren Golge <erengolge@gmail.com>
2016-11-16 14:47:50 +0200

    beautyBalanced dataste class for data sampling

--- train_scripts/beauty/simplenetver2.sh
+++ train_scripts/beauty/simplenetver2.sh
@@ -3,7 +3,7 @@ th main.lua \
 -nGPU 1 \
 -nThreads 5 \
 -data /media/eightbit/data_ssd/0.5MARGIN_ver3/ \
--dataset beautyBalanced \
+-dataset beauty \
 -nClasses 10 \
 -backend cudnn \
 -nEpochs 225 \
